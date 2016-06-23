77b224257f9cd87579ad5431f87b8ba24d8cb86e
cthtuf <gdazeysinc@gmail.com>
2016-06-23 15:12:10 +0000

    Add support of contentful to models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from contentful.cda.fields import *
+from contentful.cda.resources import *
 
 # Create your models here.
+class MainSlider(Entry):
+	__content_type__ = 'mainSlider'
+
+	imageLink = Field(Text)
+	header = Field(Text)
+	subHeader = Field(Text)
+	slogan = Field(Text)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from contentful.cda.client import *
+from main.models import *
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello World")
+	cfClient = Client('82mi4zooxljq', '7f86a46952bf2432efe06a21ed02a3d481ec0924566ff1bd11be04822d936b12', custom_entries=[MainSlider])
+	sliderRecord = cfClient.fetch(MainSlider).first()
+	return HttpResponse(sliderRecord.header)
