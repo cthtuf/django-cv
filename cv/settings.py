4545a6c6c9619b7d5a3aeddfe6e4ccc4ec5845aa
cthtuf <gdazeysinc@gmail.com>
2016-06-23 17:34:21 +0000

    Add support of templates

--- cv/settings.py
+++ cv/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'cv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'main/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from contentful.cda.client import *
-from main.models import *
+from django.template import RequestContext, loader
+from contentful.cda.client import Client
+from .models import MainSlider
 
 # Create your views here.
 def index(request):
 	cfClient = Client('82mi4zooxljq', '7f86a46952bf2432efe06a21ed02a3d481ec0924566ff1bd11be04822d936b12', custom_entries=[MainSlider])
-	sliderRecord = cfClient.fetch(MainSlider).first()
-	return HttpResponse(sliderRecord.header)
+	sliderRecords = cfClient.fetch(MainSlider).all()
+	template = loader.get_template('main/index.html')
+	context = RequestContext(request, {
+		'sliderRecords' : sliderRecords
+		})
+	return HttpResponse(template.render(context))
