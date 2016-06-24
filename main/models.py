cafb10002369fa729eda4f18830a90950376780e
cthtuf <gdazeysinc@gmail.com>
2016-06-24 13:13:51 +0000

    Add contentful models

--- main/models.py
+++ main/models.py
@@ -6,7 +6,112 @@ from contentful.cda.resources import *
 class MainSlider(Entry):
 	__content_type__ = 'mainSlider'
 
-	imageLink = Field(Text)
+	index = Field(Number)
+	image = Field(Link)
 	header = Field(Text)
 	subHeader = Field(Text)
 	slogan = Field(Text)
+
+class AboutMe(Entry):
+	__content_type__ = 'aboutMe'
+
+	fullName = Field(Text)
+	birthday = Field(Text)
+	birthPlace = Field(Text)
+	hobbies = Field(Text)
+	photos = Field(Link)
+	address = Field(Text)
+	phone = Field(Text)
+	email = Field(Text)
+	website = Field(Text)
+	shortInfo = Field(Text)
+	longInfo = Field(Text)
+
+class Awards(Entry):
+	__content_type__ = 'awards'
+
+	index = Field(Number)
+	title = Field(Text)
+	companyName = Field(Text)
+	date = Field(Text)
+	image = Field(Link)
+
+class CommonSettings(Entry):
+	__content_type__ = 'commonSettings'
+
+	descriptionSkills = Field(Text)
+	descriptionEducation = Field(Text)
+	descriptionProcess = Field(Text)
+	descriptionWorkExperience = Field(Text)
+	descriptionServices = Field(Text)
+	descriptionPortfolio = Field(Text)
+	descriptionAwards = Field(Text)
+	descriptionDownloadCv = Field(Text)
+	descriptionGetInTouch = Field(Text)
+	video = Field(Link)
+	contactAddress = Field(Text)
+	contactPhone = Field(Text)
+	contactEmail = Field(Text)
+	contactWebsite = Field(Text)
+	socialFacebook = Field(Text)
+	socialLinkedIn = Field(Text)
+	socialVk = Field(Text)
+	socialGooglePlus = Field(Text)
+
+class Education(Entry):
+	__content_type__ = 'education'
+
+	index =  Field(Number)
+	grade = Field(Text)
+	universityName = Field(Text)
+	country = Field(Text)
+	datePeriod = Field(Text)
+	description = Field(Text)
+
+class Portfolio(Entry):
+	__content_type__ = 'portfolio'
+
+	index = Field(Number)
+	header = Field(Text)
+	subHeader = Field(Text)
+	image = Field(Link)
+
+class Process(Entry):
+	__content_type__ = 'process'
+
+	index = Field(Number)
+	name = Field(Text)
+	icon = Field(Text)
+
+class Service(Entry):
+	__content_type__ = 'service'
+
+	index = Field(Number)
+	name = Field(Text)
+	description = Field(Text)
+	icon = Field(Text)
+
+class Skill(Entry):
+	__content_type__ = 'skill'
+
+	index = Field(Number)
+	name = Field(Text)
+	persent = Field(Number)
+
+class Testimonials(Entry):
+	__content_type__ = 'testimonials'
+
+	index = Field(Number)
+	name = Field(Text)
+	companyName = Field(Text)
+	text = Field(Text)
+
+class WorkExperience(Entry):
+	__content_type__ = 'workExperience'
+
+	index = Field(Number)
+	companyName = Field(Text)
+	position = Field(Text)
+	datePeriod = Field(Text)
+	description = Field(Text)
+	companyUrl = Field(Text)
--- main/views.py
+++ main/views.py
@@ -2,14 +2,52 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from contentful.cda.client import Client
-from .models import MainSlider
+from .models import *
 
 # Create your views here.
 def index(request):
-	cfClient = Client('82mi4zooxljq', '7f86a46952bf2432efe06a21ed02a3d481ec0924566ff1bd11be04822d936b12', custom_entries=[MainSlider])
-	sliderRecords = cfClient.fetch(MainSlider).all()
+	cfClient = Client(
+		'82mi4zooxljq',
+		'7f86a46952bf2432efe06a21ed02a3d481ec0924566ff1bd11be04822d936b12',
+		custom_entries= [
+			MainSlider,
+			AboutMe,
+			Awards,
+			CommonSettings,
+			Education,
+			Portfolio,
+			Process,
+			Service,
+			Skill,
+			Testimonials,
+			WorkExperience
+		]
+	)
+	mainSlider = cfClient.fetch(MainSlider).all()
+	aboutMe = cfClient.fetch(AboutMe).first()
+	awards = cfClient.fetch(Awards).all()
+	commonSettings = cfClient.fetch(CommonSettings).first()
+	education = cfClient.fetch(Education).all()
+	portfolio = cfClient.fetch(Portfolio).all()
+	process = cfClient.fetch(Process).all()
+	service = cfClient.fetch(Service).all()
+	skill = cfClient.fetch(Skill).all()
+	testimonials = cfClient.fetch(Testimonials).all()
+	workExperience = cfClient.fetch(WorkExperience).all()
+
 	template = loader.get_template('main/index.html')
 	context = RequestContext(request, {
-		'sliderRecords' : sliderRecords
-		})
+		'mainSlider' : mainSlider,
+		'aboutMe' : aboutMe,
+		'awards' : awards,
+		'commonSettings' : commonSettings,
+		'education' : education,
+		'portfolio' : portfolio,
+		'process' : process,
+		'service' : service,
+		'skill' : skill,
+		'testimonials' : testimonials,
+		'workExperience' : workExperience,
+		}
+	)
 	return HttpResponse(template.render(context))
