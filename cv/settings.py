ff3cfc37f51ae8e92651421ea3f93e95a4928951
Сергей Ярлов <sergey.yarlov@gmail.com>
2016-11-08 01:56:03 +0500

    Add support of subdomains for media and static. Close #23

--- cv/settings.py
+++ cv/settings.py
@@ -28,6 +28,7 @@ TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*', ]
 
+SITE_DOMAIN = "julia-designer.pw"
 
 # Application definition
 
@@ -147,10 +148,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
-MEDIA_URL = '/media/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
-STATIC_URL = '/static/'
+
+if not DEBUG:
+    MEDIA_URL = '//media.{0}/media/'.format(SITE_DOMAIN)
+    STATIC_URL = '//static.{0}/static/'.format(SITE_DOMAIN)
+else:
+    MEDIA_URL = '/media/'
+    STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
